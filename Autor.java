0461aedee949c6f8b8301222a6483d6e1ead3640
silvia95gb <125459353+silvia95gb@users.noreply.github.com>
2023-03-20 17:35:03 +0100

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -30,7 +30,7 @@ Libro  a = new Libro("hola", "mola");
 		this.edad = edad;	}
 
 	public String toString() {
-		return "Autor [nombre=" + nombre + ", edad=" + edad + "]";	}
+		return "Autor [name=" + nombre + ", edad=" + edad + "]";	}
 	
 	
 }
